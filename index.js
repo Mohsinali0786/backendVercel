5e0b252baafae121dd6a67cfa59ff9009ade562c
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-12-15 14:01:10 +0500

    updated v5

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json())
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, './client/build')))
 app.use(cors());
 
 // app.use('/api', require('./routes'))
